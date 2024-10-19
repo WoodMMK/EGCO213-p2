48d14045104f390987107ebb93c569b9615bb52b
WoodMMK <woodymaimongkol@gmail.com>
2024-10-19 16:12:05 +0100

    implement method for read config file

---
+++ Delivery.java
@@ -0,0 +1,53 @@
+/*
+ * Members
+ * Kunanont 115
+ * Chatpum 118
+ * Maimongkol 268
+ * Sakolkiat 273
+ */
+package Project2_6613118;
+
+import java.io.*;
+import java.util.*;
+import java.util.Random;
+import java.util.concurrent.*;
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Delivery {
+    public static void main(String args[]){
+        Delivery mainapp = new Delivery();
+        mainapp.runSimulation();
+    }
+    public Scanner readConfig(){
+        String config_filename = "config_.txt";
+        String mainPath = "src/main/java/Project2_6613118/";
+        
+        String productInput;
+        Scanner productScan = null;
+        
+        while (productScan == null) {
+            try {
+                productScan = new Scanner(new File(mainPath + config_filename));
+            } catch (FileNotFoundException e) {
+                System.err.println(e);
+                System.out.println("Enter correct file name: ");
+                config_filename = new Scanner(System.in).nextLine();
+            }
+        }
+        return productScan;
+    }
+    public void runSimulation(){
+        Scanner configScan = readConfig();
+        
+        if(configScan != null){
+            while(configScan.hasNextLine()){
+                System.out.println(configScan.nextLine());
+            }
+            configScan.close();
+        }
+        System.out.println("end simulation");
+    }
+    
+}
--- README.txt
+++ README.txt
@@ -5,3 +5,4 @@ Chatpum 118
 Maimongkol 268
 Sakolkiat 273
 
+If you're using a Maven project, place this repo inside the src/main/java directory.

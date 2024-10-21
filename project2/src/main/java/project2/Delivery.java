9de2b6464e0ee7a982360e35550a4c8b15609005
WoodMMK <woodymaimongkol@gmail.com>
2024-10-21 14:57:31 +0100

    some fleet class

--- project2/src/main/java/project2/Delivery.java
+++ project2/src/main/java/project2/Delivery.java
@@ -9,11 +9,60 @@ package project2;
 
 import java.io.*;
 import java.util.*;
-import java.util.Random;
-import java.util.concurrent.*;
-import java.util.concurrent.Semaphore;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+class Fleet{
+    final protected int max;
+    private int available;
+    
+    Fleet(int max){
+        this.max = max;
+        available = max;
+    }
+    synchronized public void Jong(int num){
+        if(available-num>=0){
+            available-=num;
+            System.out.println("current : "+available);
+        }
+        else{
+            System.out.println("No Bike left");
+        }
+        
+    }
+    public int getAvailable(){
+        return this.available;
+    }
+    public void report(){
+        System.out.println("There is "+ getAvailable()+ " cars left");
+    }
+}
+
+class BikeFleet extends Fleet{
+    BikeFleet(int num){
+        super(num);
+    }
+}
+class TruckFleet extends Fleet{
+    TruckFleet(int num){
+        super(num);
+    }
+}
+
+class MyThread extends Thread{
+    Fleet fp;
+    public MyThread(String name, Fleet fp){
+        super(name);
+        this.fp = fp;
+        this.start();
+    }
+    public void run(){
+        for(int i = 0; i<3; i++){
+           fp.Jong(2); 
+        }
+    }
+    
+}
 
 public class Delivery {
     public static void main(String []args){
@@ -21,7 +70,7 @@ public class Delivery {
         mainapp.runSimulation();
     }
     public Scanner readConfig(){
-        String config_filename = "config_.txt";
+        String config_filename = "config_1.txt";
         String mainPath = "src/main/java/project2/";
         
         String productInput;
@@ -39,9 +88,6 @@ public class Delivery {
         return productScan;
     }
     public void runSimulation(){
-
-        // this is Atom!
-        
         Scanner configScan = readConfig();
         
         if(configScan != null){
@@ -50,6 +96,21 @@ public class Delivery {
             }
             configScan.close();
         }
+        
+        BikeFleet myBikeFleet = new BikeFleet(10);
+        MyThread MT1 = new MyThread("M1", myBikeFleet);
+        MyThread MT2 = new MyThread("M2", myBikeFleet);
+        MyThread MT3 = new MyThread("M3", myBikeFleet);
+        
+        try {
+            MT1.join();
+            MT2.join();
+            MT3.join();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Delivery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
         System.out.println("end simulation");
     }
     
